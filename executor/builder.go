bca11dd3eb7680b450418470d05593bc9ef73686
robi <sulifx@gmail.com>
2018-08-06 15:00:50 +0800

    *: tableDual use fixed 1 capacity.

--- executor/builder.go
+++ executor/builder.go
@@ -1077,7 +1077,7 @@ func (b *executorBuilder) buildTableDual(v *plan.PhysicalTableDual) Executor {
 		baseExecutor: newBaseExecutor(b.ctx, v.Schema(), v.ExplainID()),
 		numDualRows:  v.RowCount,
 	}
-	v.RowCount = 1
+	e.chunkCap = 1
 	// Init the startTS for later use.
 	b.getStartTS()
 	return e
